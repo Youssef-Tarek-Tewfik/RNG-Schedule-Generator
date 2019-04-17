8b0211794725c732157b6e106f13329d3ca2a99e
Ycdm <youssefycdm@gmail.com>
2019-04-17 16:33:18 +0200

    Changes to Course.java
    split the details over Course and LessonDetails for clarity

--- Course.java
+++ Course.java
@@ -2,71 +2,21 @@ package emotionalSupport;
 
 public class Course {
     private final String id;
-    private final String name;
-    public class LessonDetails {
-        public int no_of_lecs, no_of_sections;
-        public float lec_hrs, sec_hrs;
-        public int lectureAttendees, sectionAttendees;
-		public LessonDetails(int no_of_lecs, int no_of_sections, float lec_hrs, float sec_hrs, int lectureAttendees,
-				int sectionAttendees) {
-			super();
-			this.no_of_lecs = no_of_lecs;
-			this.no_of_sections = no_of_sections;
-			this.lec_hrs = lec_hrs;
-			this.sec_hrs = sec_hrs;
-			this.lectureAttendees = lectureAttendees;
-			this.sectionAttendees = sectionAttendees;
-		}
-    }
+    public final String name;
     
-    private Doctor doctor;
-    private TeachingAssistant ta[];
+    public Doctor doctor;
+    public TeachingAssistant ta[];
+    public LessonDetails details;
     
-    public Course(String id, String name, ) {
-		this.ID = ID;
-		this.Name = Name;
-		this.No_of_lecs = No_of_lecs;
-		this.No_of_sections = No_of_sections;
-		this.No_of_labs = No_of_labs;
-		this.Lec_hrs = Lec_hrs;
-		this.Sec_hrs = Sec_hrs;
-		this.t = t;
-		this.ta = ta;
+    public Course(String id, String name, LessonDetails lessonDetails, Doctor doctor, TeachingAssistant[] TAs) {
+		this.id = id;
+		this.name = name;
+		this.details = lessonDetails;	
+		this.doctor = doctor;
+		this.ta = TAs;
     }
 
-    public String getID() {
-        return ID;
-    }
-
-    public int getNo_of_lecs() {
-        return No_of_lecs;
-    }
-
-    public String getName() {
-        return Name;
-    }
-
-    public int getNo_of_sections() {
-        return No_of_sections;
-    }
-
-    public int getNo_of_labs() {
-        return No_of_labs;
-    }
-
-    public int getLec_hrs() {
-        return Lec_hrs;
-    }
-
-    public int getSec_hrs() {
-        return Sec_hrs;
-    }
-
-    public int getLab_hrs() {
-        return Lab_hrs;
-    }
-
-    public int getNo_of_TAs() {
-        return No_of_TAs;
-    }
+	public String getID() {
+		return id;
+	}
 }
---
+++ LessonDetails.java
@@ -0,0 +1,17 @@
+package emotionalSupport;
+
+public class LessonDetails {
+    public int no_of_lecs, no_of_sections;
+    public float lec_hrs, sec_hrs;
+    public int lectureAttendees, sectionAttendees;
+    
+	public LessonDetails(int no_of_lecs, int no_of_sections, float lec_hrs, float sec_hrs, int lectureAttendees,
+			int sectionAttendees) {
+		this.no_of_lecs = no_of_lecs;
+		this.no_of_sections = no_of_sections;
+		this.lec_hrs = lec_hrs;
+		this.sec_hrs = sec_hrs;
+		this.lectureAttendees = lectureAttendees;
+		this.sectionAttendees = sectionAttendees;
+	}
+}
